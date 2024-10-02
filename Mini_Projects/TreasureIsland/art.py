8683bc756fb39d06db45e6140e13766fb97a1078
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-10-02 15:36:21 -0700

    Create art.py

---
+++ Mini_Projects/TreasureIsland/art.py
@@ -0,0 +1,188 @@
+#Lets make the program more interesting
+#below are all the pictures to be used
+welcome = '''
+ _                                     _     _                 _ 
+| |                                   (_)   | |               | |
+| |_ _ __ ___  __ _ ___ _   _ _ __ ___ _ ___| | __ _ _ __   __| |
+| __| '__/ _ \/ _` / __| | | | '__/ _ \ / __| |/ _` | '_ \ / _` |
+| |_| | |  __/ (_| \__ \ |_| | | |  __/ \__ \ | (_| | | | | (_| |
+ \__|_|  \___|\__,_|___/\__,_|_|  \___|_|___/_|\__,_|_| |_|\__,_|
+                                                                 
+'''
+
+
+treasure = '''
+YOU WON!!! You entered into room full of treasure!!
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/[TomekK]
+*******************************************************************************
+'''
+
+
+boat = ''' .  o ..                  
+
+     o . o o.o                
+          ...oo               
+            __[]__            
+         __|_o_o_o\__         
+         \""""""""""/         
+          \. ..  . /          
+     ^^^^^^^^^^^^^^^^^^^^   
+A boat showed up while you were waiting and took you to the island
+'''
+
+beast = '''   
+You stepped into Beast's room and you were eaten alive! GAME OVER! :(              
+                   (    )
+                  ((((()))
+                  |o\ /o)|
+                  ( (  _')
+                   (._.  /\__
+                  ,\___,/ '  ')
+    '.,_,,       (  .- .   .    )
+     \   \\     ( '        )(    )
+      \   \\    \.  _.__ ____( .  |
+       \  /\\   .(   .'  /\  '.  )
+        \(  \\.-' ( /    \/    \)
+         '  ()) _'.-|/\/\/\/\/\|
+             '\\ .( |\/\/\/\/\/|
+               '((  \    /\    /
+               ((((  '.__\/__.')
+                ((,) /   ((()   )
+                 "..-,  (()("   /
+            pils  _//.   ((() ."
+          _____ //,/" ___ ((( ', ___
+                           ((  )
+                            / /
+                          _/,/'
+                        /,/,"
+'''
+
+shark = '''
+               ,
+             .';
+         .-'` .'
+       ,`.-'-.`\
+      ; /     '-'
+      | \       ,-,
+      \  '-.__   )_`'._
+       '.     ```      ``'--._
+      .-' ,                   `'-.
+       '-'`-._           ((   o   )
+        jgs   `'--....(`- ,__..--'
+                       '-'`
+
+You were attacked by a shark! GAME OVER!!!
+'''
+
+game_over = '''
+  _____                         ____                 
+ / ____|                       / __ \                
+| |  __  __ _ _ __ ___   ___   | |  | |_   _____ _ __ 
+| | |_ |/ _` | '_ ` _ \ / _ \  | |  | \ \ / / _ \ '__|
+| |__| | (_| | | | | | |  __/  | |__| |\ V /  __/ |   
+ \_____|\__,_|_| |_| |_|\___|   \____/  \_/ \___|_|  
+'''
+
+
+train = '''
+
+                 _-====-__-======-__-========-_____-============-__
+               _(                                                 _)
+            OO(   You were eaten alive by the zombies in the train  )_
+           0  (_                                                   _)
+         o0     (_                                                _)
+        o         '=-___-===-_____-========-___________-===-dwb-='
+      .o                                _________
+     . ______          ______________  |         |      _____
+   _()_||__|| ________ |            |  |_________|   __||___||__
+  (BNSF 1995| |      | |            | __Y______00_| |_         _|
+ /-OO----OO""="OO--OO"="OO--------OO"="OO-------OO"="OO-------OO"=P
+#####################################################################
+
+GAME OVER
+'''
+
+
+bus = '''
+You boarded the bus that took you away from your destination
+                        .---------------------------.
+                       /,--..---..---..---..---..--. `.
+                      //___||___||___||___||___||___\_|
+                      [j__ ######################## [_|
+                         \============================|
+                      .==|  |"""||"""||"""||"""| |"""||
+                     /======"---""---""---""---"=|  =||
+                     |____    []* GAME OVER____  | ==||
+                     //  \\               //  \\ |===||  
+                     "\__/"---------------"\__/"-+---+'
+
+'''
+
+seabus = '''
+             .
+             _\____
+            |_===__`.        ==/
+            \/  '---"\ _ _ _ _/
+      ______/_______/_|_|_|_|_|
+    _|---------------------==.`
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Sea bus took you to Mystery Island!
+'''
+
+castle = '''
+On this island is this castle with 3 doors, Behind the doors is the treasure or the danger!
+
+
+           M...m...m...M                              M...m...m...M
+            MmmmmMmmmmM                                MmmmmMmmmmM
+            MMmmMx""xMM                                MMx""xMmmMM
+            Mmmmx    xM                                Mx    xmmmM
+            MMmmx    xM                                Mx    xmmMM
+   ss sss ssMmmmxxxxxxMsss  sss  sss  sss  sss  sss  ssMxxxxxxmmmMss sss sss sss s
+   SS SSS SSMMx""xMmmMMSSS  SSS  SSS  SSS  SSS  SSS  SSMMmmMx""xMMSS SSS SSS SSS S
+   SSssSSSssMx    xmmmMsssSSsssSSsssSSsssSSsssSSsssSSssMmmmx    xMSSsssSSsssSSsssS
+   ssSSsssSSMx    xmmMMss""Sss""Sss""Sss""Sss""Sss""SssMMmmx    xMssSSSssSSSssSSSs
+   SSssSSSssMxxxxxxmmmMSx  xSx  xSx  xSx  xSx  xSx  xSSMmmmxxxxxxMSSSssSSSssSSSssS
+   ssSSsssSSMMmmMMMmmMMsxxxxsxxxxsxxxxsxxxxsxxxxsxxxxssMMmmMMMmmMMssSSSssSSSssSSSs
+   SSssSSSssMmmmmMmmmmMSSsssSSsssSSs".mmm."SSsssSSsssSSMmmmmMmmmmMSSsssSSsssSSsssS
+   ssSSsssSSMMmmMMMmmMMssSSSssSSSss"mMMMMMm"sSSSssSSSssMMmmMMMmmMMssSSSssSSSssSSSs
+   SSssSSSssMmmmmMmmmmMSSsssSSsssSS.MMMMMMM.SsssSSsssSSMmmmmMmmmmMSSsssSSsssSSsssS
+   ssSSsssSSMMmmMMMmmMMssSSSssSSSss.MMMMMMM.sSSSssSSSssMMmmMMMmmMMssSSSssSSSssSSSs
+   SSssSSSsmMMMMMMMMMMMmSSSSSSSSSSS.MMMMMMM.SSSSSSSSSSmMMMMMMMMMMMmSsssSSsssSSssss
+           MMMMMMMMMMMMM              ..              MMMMMMMMMMMMM           fsc
+                                    :.
+                                     ":.
+                                     .:::.
+
+'''
+
+
+red_door = '''
+You stepped into a door full of fire! GAME OVER!
+
+               (  .      )
+           )           (              )
+                 .  '   .   '  .  '  .
+        (    , )       (.   )  (   ',    )
+         .' ) ( . )    ,  ( ,     )   ( .
+      ). , ( .   (  ) ( , ')  .' (  ,    )
+     (_,) . ), ) _) _,')  (, ) '. )  ,. (' )
+ jgs^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+'''

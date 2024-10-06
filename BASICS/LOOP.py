58a4543c1115a427663db8cbd3169c7892d08205
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-10-05 20:44:38 -0700

    Create LOOP.py

---
+++ BASICS/LOOP.py
@@ -0,0 +1,15 @@
+#Loop is a way of repeating an action until a given condition is met
+#for example lets create a list of fruits and using for loop, to print each element of the list.
+
+#create a list called fruits
+fruits = ["Apple", "Banana", "Grapes", "Mango"]
+
+#adding loop to print each element of the list "fruits", unless every element is printed
+for fruit in fruits:
+  print(fruit)   
+
+#RESULT: 
+#Apple
+#Banana
+#Grapes
+#Mango

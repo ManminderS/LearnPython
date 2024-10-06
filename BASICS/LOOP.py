2bca045548ff643ee76adcf2e7f00cd3de772968
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-10-05 21:41:46 -0700

    Update LOOP.py

--- BASICS/LOOP.py
+++ BASICS/LOOP.py
@@ -13,3 +13,23 @@ for fruit in fruits:
 #Banana
 #Grapes
 #Mango
+
+#Indentation is important, anything after for loop statement is part of for loop
+for fruit in fruits:
+  print(fruit)  #part of for loop
+print(fruits)   #not a part of the loop as there is no indentation in front of it.
+
+
+#example 2: consider a list of scores
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+
+#first way of printing the total of scores in the student_scores
+sum = sum(student_scores)
+print(sum) #RESULT: 2068
+
+#Getting the same result with for-loop
+sum = 0
+for score in student_scores:
+    sum += score
+print(sum) #RESULT: 2068
+

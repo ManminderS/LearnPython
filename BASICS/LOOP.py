ea655b39e95247eafe0c390cd359f8470faec6e5
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-10-07 13:49:43 -0700

    Update LOOP.py

--- BASICS/LOOP.py
+++ BASICS/LOOP.py
@@ -13,13 +13,13 @@ for fruit in fruits:
 #Banana
 #Grapes
 #Mango
-
+-------------------------------------------------------------------------------------------------------------------
 #Indentation is important, anything after for loop statement is part of for loop
 for fruit in fruits:
   print(fruit)  #part of for loop
 print(fruits)   #not a part of the loop as there is no indentation in front of it.
 
-
+--------------------------------------------------------------------------------------------------------------------
 #example 2: consider a list of scores
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
 
@@ -33,6 +33,7 @@ for score in student_scores:
     sum += score
 print(sum) #RESULT: 2068
 
+----------------------------------------------------------------------------------------------------------------------
 #scenario
 #find out the biggest number from the following list
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
@@ -45,4 +46,27 @@ for score in student_scores:
     if score > max_num:
         max_num = score
 print(max_num)
+
+---------------------------------------------------------------------------------------------------------------------------
+#LEARNING 2: Using range function with For-Loop
+#Syntax : for i in range(a,b)    #i: particular elements #a: starting range and b: ending range, a =< num < b
+
+#example: dislay 1-10 numbers on screen
+#Solution:
+for num in range(1,11):
+  print(num)
+
+#Result: 
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+--------------------------------------------------------------------------------------------------------------------------
 

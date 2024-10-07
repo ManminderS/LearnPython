d299ba8fa2f0315465b9deb1a5f595a201637d61
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-10-06 20:25:31 -0700

    Update LOOP.py

--- BASICS/LOOP.py
+++ BASICS/LOOP.py
@@ -33,3 +33,16 @@ for score in student_scores:
     sum += score
 print(sum) #RESULT: 2068
 
+#scenario
+#find out the biggest number from the following list
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+
+#declaring variable to record the max number from the list
+max_num = 0
+
+#for loop
+for score in student_scores:
+    if score > max_num:
+        max_num = score
+print(max_num)
+

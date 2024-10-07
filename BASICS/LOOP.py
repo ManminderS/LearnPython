e475bb312b7ebd1aa530501cdac2b5374a9e1245
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-10-07 16:08:18 -0700

    Update LOOP.py

--- BASICS/LOOP.py
+++ BASICS/LOOP.py
@@ -83,6 +83,25 @@ for num in range(1,101):
         num = "FIZZ"
     print(num)
 
+---or--- Alternative Solution
+# Create a loop with For and Range to go from 1 to 100.
+for number in range(1, 101):
+  # First check if the number is divisible by both 3 and 5.
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+ 
+  # Then check if the number is only divisible by 3
+  elif number % 3 == 0:
+    print("Fizz")
+ 
+  # Finally check if the number is only divisible by 5
+  elif number % 5 == 0:
+    print("Buzz")
+ 
+  # If it's not divisible by either of those numbers, just print the number
+  else:
+    print(number)
+
 #Result:
 1
 2

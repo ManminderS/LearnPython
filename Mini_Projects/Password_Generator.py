6149a75881ba91c8d9819ff1b62b36f8fbe7d4a4
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-10-09 13:25:24 -0700

    Update Password_Generator.py

--- Mini_Projects/Password_Generator.py
+++ Mini_Projects/Password_Generator.py
@@ -1,6 +1,8 @@
--------WIP-----
 #import the important module
 import random
+
+#Welcome Screen
+print("Welcome to Strong password generator")
 
 #Different lists
 letter = ['a', 'b', 'c','d','e', 'f','g','h','i','j','k']
@@ -12,21 +14,29 @@ nr_letter = int(input("How many letters do you want"))
 nr_num = int(input("How many numbers do you want"))
 nr_symbol = int(input("How many symbol do you want"))
 
+#Create an empty list called password, this will be used to store the random elements from the lists(letter, number and symbol)
 password = []
 
 #APPEND RANDOM ELEMENTS FROM THE LISTS ABOVE INTO A NEW LIST CALLED password
-#random.sample(list,number of elements to get) randomly selects asked number of values from the given list
+#random.sample(listname_from_where_to_get_the_element,user_input_from_nr_letter), as a result this randomly selects asked number of values from the given list
 random_letter = random.sample(letter,nr_letter)
+
+#randomly add element from list "number" to list random_number
 random_number = random.sample(number,nr_num)
+
+#randomly add element from list "symbol" to list random_symbol
 random_symbol = random.sample(symbol,nr_symbol)
 
-#append these n random elements in the list password
+#Add the elements from random_letter, random_symbol, and random_symbol into list password
 password.extend(random_letter)
 password.extend(random_number)
 password.extend(random_symbol)
 
+#shuffle the elements in list "password"
 random.shuffle(password)
 
+#use the for loop to pick the element from list "password".
 for item in password:
+    #print each element next to each other without spacing
     print(item, end='')
 

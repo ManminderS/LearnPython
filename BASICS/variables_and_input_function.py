f57c1764b8f391430f9bd7f266e3e48b64846c7f
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-10-14 21:22:54 -0700

    Create variables_and_input_function.py

---
+++ BASICS/variables_and_input_function.py
@@ -0,0 +1,7 @@
+#Variables is a way to assign a value to a variable
+#for example lets declare a variable 'value' and assign the value 10 to it.
+value = 10
+
+#lets print the value of the variable
+print(value)
+

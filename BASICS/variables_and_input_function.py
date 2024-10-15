db123768bbb399b40815f1a2371698162e8cd740
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-10-14 21:29:59 -0700

    Update variables_and_input_function.py

--- BASICS/variables_and_input_function.py
+++ BASICS/variables_and_input_function.py
@@ -5,3 +5,11 @@ value = 10
 #lets print the value of the variable
 print(value)
 
+-------------------------------------------------------------------------
+# Program to input name and displaying name and lenth of the name
+username=input("What is your name?")
+print("Hi",(username),"!")
+length=len(username)
+print("Total characters in your name are",(length),".")
+
+-------------------------------------------------------------------------

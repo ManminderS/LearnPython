d26c47a722827a8b167f810404e38e44a00e952e
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-10-15 16:25:22 -0700

    Create Functions.py

---
+++ BASICS/Functions.py
@@ -0,0 +1,30 @@
+Official Documentation: https://docs.python.org/3/library/functions.html
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Python has various in-built functions such as len(), int(), print() etc..
+# But it is also possible to create your own functions in Python.
+
+------------------------
+#General Syntax, action under the defined functions must be intended in order for them to be executed.
+def function_name():
+  do this
+  do that
+  and do that
+
+-----------------------------------------------------------------------------------------------
+#creating a function with below syntax, my_function is the name of the function we are creating
+def my_function():
+  #add action to be performed when this function is called.
+  print("Hello")
+  print("Bye")
+
+#lets call the function:
+my_function()
+
+---------
+#Results: 
+#Hello
+#Bye
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+

b3ed650c778e18ca8c7bd8ced030abff0c709e2c
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-10-22 16:22:27 -0700

    Update Functions.py

--- BASICS/Functions.py
+++ BASICS/Functions.py
@@ -4,7 +4,7 @@ Official Documentation: https://docs.python.org/3/library/functions.html
 # Python has various in-built functions such as len(), int(), print() etc..
 # But it is also possible to create your own functions in Python.
 
-------------------------
+--------------------------
 #General Syntax, action under the defined functions must be intended in order for them to be executed.
 def function_name():
   do this
@@ -21,7 +21,7 @@ def my_function():
 #lets call the function:
 my_function()
 
----------
+-----------
 #Results: 
 #Hello
 #Bye

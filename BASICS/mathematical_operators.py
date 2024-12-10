740c6193db537a67dafcdcef9a95211e5a9bc874
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-12-10 14:08:36 -0800

    Create mathematical_operators.py

---
+++ BASICS/mathematical_operators.py
@@ -0,0 +1,34 @@
+'''
+1. WHat are mathematical operators in Python?
+A mathematical operator is a math symbol or function used to perform mathematical operation on values(operands)
+
+Different types of Mathematical Operators.
+a. Arithematic Operator: used for basic arithmetic
+    Addition (+): 5 + 3 = 8
+    Subtraction (-): 5 - 3 = 2
+    Multiplication (*): 5 * 3 = 15
+    Division (/): 5 / 2 = 2.5(Gives the quotient)
+    Modulus (%): 5 % 2 = 1(Gives the remainder)
+    Floor Division (//): 5 // 2 = 2(Gives the higher value from Quotient)
+    Exponentiation (**): 5 ** 2 = 25
+
+b. Relational/Comparison Operators: Compare two values.
+Examples:
+Greater than (>): 5 > 3 → True
+Less than (<): 3 < 5 → True
+
+c. Logical Operators: Combine conditional statements.
+Examples:
+AND (and): True and False → False
+OR (or): True or False → True
+
+d. Assignment Operators:Assign values.
+Example:
+Equal (=): x = 5
+Bitwise Operators:
+
+e. Perform operations on bits.
+Example:
+AND (&): 5 & 3 evaluates to 1.
+
+'''

8ef8ec2ec9e442183995dafe8b2e038590639095
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-12-16 16:24:38 -0800

    Update return_prime_numbers_between_1_&_num_given_by_the_user.py

--- Practise/return_prime_numbers_between_1_&_num_given_by_the_user.py
+++ Practise/return_prime_numbers_between_1_&_num_given_by_the_user.py
@@ -1,1 +1,21 @@
+#Write a program that:
+#1. Takes a number N as input.
+#2. Prints all the prime numbers between 1 and N.
+
+#take input from the user
+user_input = int(input("Enter a positive number"))
+
+#VALIDATE THE INPUT
+#1. Validate for null value
+if not user_input:
+    print("Error, Please enter the number")
+elif user_input <= 0:
+    print("Error, Number should be above 1")
+
+
+
+#calculate the prime number between 1 and the number given by the user
+
+
+#Display the result
 

f5930cfc913256537258bb9da40d56b481006841
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-12-17 15:50:29 -0800

    Update return_prime_numbers_between_1_&_num_given_by_the_user.py

--- Practise/return_prime_numbers_between_1_&_num_given_by_the_user.py
+++ Practise/return_prime_numbers_between_1_&_num_given_by_the_user.py
@@ -5,17 +5,22 @@
 #take input from the user
 user_input = int(input("Enter a positive number"))
 
-#VALIDATE THE INPUT
-#1. Validate for null value
-if not user_input:
-    print("Error, Please enter the number")
-elif user_input <= 0:
-    print("Error, Number should be above 1")
+# #VALIDATE THE INPUT(WIP as I havent learned while and other concepts that can help me with this one yet, 
+#will tackle this once the main part of the program is done)
+# #1. Validate for null value
+#     if not user_input.strip():
+#         print("No Input Found, Please enter the number")
+#     elif user_input < 0:
+#         print("Error, Number should be posiitve")
 
 
 
-#calculate the prime number between 1 and the number given by the user
-
+#calculate the prime number between 1 and the number given by the user.
+#prime numbers are the numbers divisble by 1 and divisible by the number itself.
+for num in range(2,user_input):
+    if num % 1 == 0 & num 
+    
+        
+    
 
 #Display the result
-

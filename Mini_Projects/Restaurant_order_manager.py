e6470c6bce476079dc1b6423bd8534a4fab29cca
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-12-29 14:01:47 -0800

    Update Restaurant_order_manager.py

--- Mini_Projects/Restaurant_order_manager.py
+++ Mini_Projects/Restaurant_order_manager.py
@@ -1,1 +1,17 @@
+#import modules
+
+
+#Welcome message
+print("\n..........Welcome to the order management..........\n")
+
+#Display the master page:
+print("Please select one of the following options:")
+choice = int(input("1. Display Menu \n2. Place an Order \n3. End Session \ntype your choice here: "))
+
+#logic to display the menu.
+if choice == 1:
+    print("\n1. Spaghetti - $12.99 (Main Course \n2. Salad - $6.99 (Starter) "
+          "\n3. Cheesecake - $4.99 (Dessert) \n4. Pizza - $10.99 (Main Course)")
+else:
+    print("You haven't selected any option to go further")
 

664655cc5a3a5c30e8a56cc9676a26709bbb5201
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-12-29 13:33:31 -0800

    Update class_result.py
    modified code for adding features to my code.

--- Practise/class_result.py
+++ Practise/class_result.py
@@ -1,10 +1,12 @@
 #Class Performance
+print("\n")
+print(".........Welcome to the Class result 2025.......... \n \nFollow the instructions below \u2193 ")
 
 #input to get the grades from the teacher
-grades = input("Please enter the grades separated by space: ").split()
+grades = input("Please enter the overall percentage of the students in your class separated by space %age: ").split()
 
 #input test
-print(f"Output of input from the teacher is {grades}")
+# print(f"Output of input from the teacher is {grades}")
 
 #convert the values of the grades into list of int values
 grades = [int(grade) for grade in grades]
@@ -19,13 +21,13 @@ for grade in grades:
     else:
         fail_list.append(grade)
 
-print(f"Pass List: {pass_list}")
-print(f"Fail List: {fail_list}")
+# print(f"Pass List: {pass_list}")
+# print(f"Fail List: {fail_list}")
 
 #Percentage each student represent in the class
 total_students = len(grades)
 percentage_per_student = round(100/total_students, 1)
-print(f"each student represents: {percentage_per_student}%")
+# print(f"each student represents: {percentage_per_student}%")
 
 #How may percentage of people passed and failed
 total_students_passed = len(pass_list)*percentage_per_student
@@ -34,4 +36,4 @@ total_students_failed = len(fail_list)*percentage_per_student
 if total_students_passed > total_students_failed:
     print(f"Good Job Class, {total_students_passed}% students passed")
 else:
-    print("Work Harder Class")
+    print("Work Harder Class, Majority of you are fail")

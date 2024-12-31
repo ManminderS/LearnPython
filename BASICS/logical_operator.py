399826313001f93f62351875ef2fb20936e0bfc3
MavSingh <86635253+ManminderS@users.noreply.github.com>
2024-12-31 15:04:34 -0800

    Create logical_operator.py

---
+++ BASICS/logical_operator.py
@@ -0,0 +1,40 @@
+#What are Logical operators?
+# Logical operators are used to combine conditional statements and control the flow of your program. 
+# They evaluate expressions and return True or False based on the logic they implement.
+
+..........................................
+#types of operators
+#AND
+#OR
+#not
+
+------------------------------------------------
+Truth Tables(AND)
+Operand 1	  Operand 2	   Result 
+True	       True	     ->True
+True	       False	   ->False
+False	       True      ->False
+False	       False	   ->False
+
+or Truth Table
+Operand 1	Operand 2	Result (Operand1 or Operand2)
+True	True	True
+True	False	True
+False	True	True
+False	False	False
+not Truth Table
+Operand	Result (not Operand)
+True	False
+False	True
+
+...............................................
+#HOW TO USE THE OPERATORS?
+
+Condition_1 OR Condition_2 = either true or false
+
+#example 1: AND
+
+a = 3
+b = 5
+
+if a>1 and a<4

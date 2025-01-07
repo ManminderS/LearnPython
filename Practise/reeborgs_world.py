4900a05d77adc0ccafcab83209805d417f4f4728
MavSingh <86635253+ManminderS@users.noreply.github.com>
2025-01-06 16:10:13 -0800

    Update reeborgs_world.py

--- Practise/reeborgs_world.py
+++ Practise/reeborgs_world.py
@@ -18,4 +18,53 @@ def jump_hurdle():
     
 for hurdles in range(6):
     jump_hurdle()
+
+-------------------------------------------------------
+#2. Solving the reeborg Hurdle 1 using the while loop
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
     
+def jump_hurdle():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+#for hurdles in range(6):
+ #   jump_hurdle()
+
+no_of_hurdles = 6
+while no_of_hurdles > 0:
+    jump_hurdle()
+    no_of_hurdles -= 1
+
+--------------------------------------------------
+#3. Solving reeborg Hurdle 2 using the while loop
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump_hurdle():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+#for hurdles in range(6):
+ #   jump_hurdle()
+
+while at_goal() != True:
+    jump_hurdle()

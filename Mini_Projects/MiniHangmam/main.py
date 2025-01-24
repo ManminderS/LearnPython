ef0a6c007e18c0000988f9e4a4108e90bcbb6183
MavSingh <86635253+Mav-Singh@users.noreply.github.com>
2025-01-24 15:52:44 -0800

    Create main.py

---
+++ Mini_Projects/MiniHangmam/main.py
@@ -0,0 +1,11 @@
+#starting
+print("Welcome to hangman")
+
+#words
+word = ["batman", "superman", "Shaktimaan"]
+
+#display random word
+random_word = random.choice(word)
+
+#print random word
+print(random_word)

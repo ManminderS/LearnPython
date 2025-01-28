3ece92141ca6ad0aaa3bf3e43dc8a02e9f7350eb
MavSingh <86635253+Mav-Singh@users.noreply.github.com>
2025-01-28 15:55:51 -0800

    Update main.py

--- Mini_Projects/MiniHangmam/main.py
+++ Mini_Projects/MiniHangmam/main.py
@@ -7,7 +7,7 @@ word_list = ["aardvark", "baboon", "camel"]
 # TODO-1 Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 chosen_word = random.choice(word_list)
 print(f"The chosen word is: {chosen_word}")
-
+chosen_word_list = list(chosen_word)
 
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Please enter a letter to guess: ").lower()
@@ -15,7 +15,8 @@ guess = input("Please enter a letter to guess: ").lower()
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
 #  is, "Wrong" if it's not.
-if guess in chosen_word:
-    print("Right")
-else:
-    print("Wrong")
+for char in chosen_word_list:
+    if char == guess:
+        print("right")
+    else:
+        print("wrong")

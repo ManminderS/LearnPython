8581935adbda37b1b7a885ba03b4ddaf29c4fa3a
MavSingh <86635253+Mav-Singh@users.noreply.github.com>
2025-01-30 14:10:29 -0800

    Update main.py

--- Mini_Projects/MiniHangmam/main.py
+++ Mini_Projects/MiniHangmam/main.py
@@ -1,13 +1,42 @@
+----------------This is fixed version--------------------------------
+
 #importing modules
 import random
 
 #wordlists
-word_list = ["aardvark", "baboon", "camel"]
+word_list = ["batman", "Superman", "Avengers", "Hulk", "Transformers"]
 
 # TODO-1 Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 chosen_word = random.choice(word_list)
-print(f"The chosen word is: {chosen_word}")
+length = len(chosen_word)
 
+#Make placeholders for the chosen word
+#create a empty list "spaces" & Insert the _ that is equal to lenght of the chosen word
+spaces = []
+for element in range(length):
+    spaces.append("_")
+
+
+#join the elements of the list and display it
+the_word = " ".join(spaces)
+print(the_word)
+
+#Ask user to guess the letter:
+guess = input("Guess the letter: ")
+
+
+#join the elements of the list and display it
+the_word = " ".join(spaces)
+print(the_word)
+
+#when user choose a word, if it matches any alphabet in the word, replace it with the guessed word
+if guess in chosen_word:
+    print("R")
+else:
+    print("W")
+
+
+-----------------------------------------this is previous code----------------------------------------------------------------------
 #placeholder
 placeholders = len(chosen_word)*"_ "
 placeholders = [placeholders]
@@ -28,3 +57,29 @@ for letter in chosen_word:
         print("Wrong")
         
 #put the the right guessed letter into the place holder
+
+
+
+--------------------------this is what i just did on Jan 30, 2025--------------------
+#wordlists
+word_list = ["aardvark", "baboon", "camel"]
+
+# TODO-1 Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+chosen_word = random.choice(word_list)
+print(f"The chosen word is: {chosen_word}")
+length = len(chosen_word)
+
+#make spaces = length of chosen word into a string
+spaces = []
+
+for element in range(length):
+    spaces.append("_")
+print(spaces)
+
+#make place holders
+# placeholder = list(chosen_word)
+# print(placeholder)
+
+#print the elements in the list
+the_word = " ".join(spaces)
+print(the_word)

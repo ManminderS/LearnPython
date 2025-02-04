efd09fe6c5e08f70c0ee3148e551adee6c4debbe
MavSingh <86635253+Mav-Singh@users.noreply.github.com>
2025-02-04 13:34:29 -0800

    add alternative correct code

--- Mini_Projects/MiniHangmam/main.py
+++ Mini_Projects/MiniHangmam/main.py
@@ -38,50 +38,31 @@ print(display)
 
 
 
------------------------------------------this is previous code----------------------------------------------------------------------
-#placeholder
-placeholders = len(chosen_word)*"_ "
-placeholders = [placeholders]
-print(placeholders)
-print(len(placeholders))
+-----------------------------------------this is alternative correct code----------------------------------------------------------------------
+
+#import modules
+import random
+
+#wordlist
+words = ['batman', 'shaktimaan', 'transformers']
+
+#randomly choose a word
+chosen_word = random.choice(words)
+print(chosen_word)
+
+#placeholder for chosen word
+placeholder = ["_"]*len(chosen_word)
+print(f"Guess the word: {' '.join(placeholder)}")
+
+#while loop to keep asking the user to keep guessing the word unless the 
+while "_" in placeholder:
+    guess = input("Please enter a letter to start guessing: ").lower()
+    
+    for i in range(len(chosen_word)):
+        if chosen_word[i] == guess:
+            placeholder[i] = guess
+    print(''.join(placeholder))
 
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Please enter a letter to guess:").lower()
 
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
-for letter in chosen_word:
-    if letter == guess:
-        print("right")
-    else:
-        print("Wrong")
-        
-#put the the right guessed letter into the place holder
-
-
-
---------------------------this is what i just did on Jan 30, 2025--------------------
-#wordlists
-word_list = ["aardvark", "baboon", "camel"]
-
-# TODO-1 Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
-chosen_word = random.choice(word_list)
-print(f"The chosen word is: {chosen_word}")
-length = len(chosen_word)
-
-#make spaces = length of chosen word into a string
-spaces = []
-
-for element in range(length):
-    spaces.append("_")
-print(spaces)
-
-#make place holders
-# placeholder = list(chosen_word)
-# print(placeholder)
-
-#print the elements in the list
-the_word = " ".join(spaces)
-print(the_word)
